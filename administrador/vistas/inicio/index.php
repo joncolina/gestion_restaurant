2eaca40f57a6364447b91826493ee928638fc262
JonathanWeb <joncolina@hotmail.com>
2020-06-04 01:11:31 -0400

    Interfaz "index" - Ambiente ADMIN

---
+++ administrador/vistas/inicio/index.php
@@ -0,0 +1,78 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+<!-- Animate.css -->
+<link rel="stylesheet" href="recursos/administrador/css/template.css">
+
+<div class="container-fluid">
+	<div class="row">
+	<div class="col-6 col-md-4">
+	<div class="wrimagecard wrimagecard-topimage">
+          <a href="#">
+          <div class="wrimagecard-topimage_header">
+            <center class="icon_interfaz"> <i class="fas fa-utensils"></i></center>
+          </div>
+          <div class="wrimagecard-topimage_title">
+            <h4 class="centrar">Restaurantes
+            <div class="pull-right badge"> </div></h4>
+          </div>
+        </a>
+      </div>
+      </div>
+    <div class="col-6 col-md-4">
+      <div class="wrimagecard wrimagecard-topimage">
+          <a href="#">
+          <div class="wrimagecard-topimage_header" >
+            <center class="icon_interfaz"><i class = "fas fa-handshake"></i></center>
+          </div>
+          <div class="wrimagecard-topimage_title">
+            <h4 class="centrar">Afiliar Restaurante
+            <div class="pull-right badge" id="WrControls"></div></h4>
+          </div>
+        </a>
+      </div>
+</div>
+	<div class="col-6 col-md-4">
+      <div class="wrimagecard wrimagecard-topimage">
+          <a href="#">
+          <div class="wrimagecard-topimage_header">
+             <center class="icon_interfaz"><i class="fas fa-cogs"></i></center>
+          </div>
+          <div class="wrimagecard-topimage_title">
+            <h4 class="centrar">Configuraciones de sistema
+            <div class="pull-right badge" id="WrGridSystem"></div></h4>
+          </div>
+          
+        </a>
+      </div>
+    </div>
+    <div class="col-6 col-md-4">
+      <div class="wrimagecard wrimagecard-topimage">
+          <a href="#">
+          <div class="wrimagecard-topimage_header">
+            <center class="icon_interfaz"><i class="fas fa-users-cog"></i></center>
+          </div>
+          <div class="wrimagecard-topimage_title" >
+            <h4 class="centrar">Gestión de usuarios
+            <div class="pull-right badge" id="WrForms"></div>
+            </h4>
+          </div>
+          
+        </a>
+      </div>
+	</div>
+	<div class="col-6 col-md-4">
+      <div class="wrimagecard wrimagecard-topimage">
+          <a href="#">
+          <div class="wrimagecard-topimage_header" style="background-color:  rgba(250, 188, 9, 0.1)">
+             <center class="icon_interfaz"><i class="fas fa-user-secret"></i></center>
+          </div>
+          <div class="wrimagecard-topimage_title">
+            <h4 class="centrar">Gestión de perfiles
+            <div class="pull-right badge" id="WrInformation"></div></h4>
+          </div>
+         
+        </a>
+      </div>
+	</div>
+
+</div>
+</div>
